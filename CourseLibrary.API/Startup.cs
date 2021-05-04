2ba8b8f27dff57aee2c456a5404a22bca59e2c0e
André Almeida <andreclalmeida@gmail.com>
2021-05-04 15:44:45 -0300

    Supporting HEAD

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -23,7 +23,8 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(setupAction => {
+            services.AddControllers(setupAction =>
+            {
                 setupAction.ReturnHttpNotAcceptable = true;
             }).AddXmlDataContractSerializerFormatters();
 
