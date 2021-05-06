d2d4588bd688b8bb64d30869fa18256ac7a34189
André Almeida <andreclalmeida@gmail.com>
2021-05-06 13:36:17 -0300

    Class-Level Input Validation with Custom Attribute

---
+++ CourseLibrary.API/ValidationAttributes/CourseValidation.cs
@@ -0,0 +1,25 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var createCourseDto = (CreateCourseDto)validationContext.ObjectInstance;
+
+            if (createCourseDto.Title == createCourseDto.Description)
+            {
+                return new ValidationResult("The provided description should be different from the title",
+                    new[] { nameof(CreateCourseDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
