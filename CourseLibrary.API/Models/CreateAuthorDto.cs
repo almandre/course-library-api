ed7fe721e1985ca8602dece3e4ab32ccb3874bce
André Almeida <andreclalmeida@gmail.com>
2021-05-06 13:12:07 -0300

    Validating Input with Data Annotation at CreateAuthorDto.

--- CourseLibrary.API/Models/CreateAuthorDto.cs
+++ CourseLibrary.API/Models/CreateAuthorDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,19 @@ namespace CourseLibrary.API.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
 
+        [Required]
         public DateTime DateOfBirth { get; set; }
 
+        [Required]
+        [MaxLength(50)]
         public string MainCategory { get; set; }
 
         public ICollection<CreateCourseDto> Courses { get; set; }
